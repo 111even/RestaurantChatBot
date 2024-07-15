29c37e3ead82ed801bc69b9cdf854b19f5b2b85c
Is'haq Abdulrazzaq <ishaqat111@gmail.com>
2024-07-15 15:28:56 +0100

    Modified app.listen to 0.0.0.0

--- src/app.js
+++ src/app.js
@@ -11,5 +11,5 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use('/', chatbotRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
