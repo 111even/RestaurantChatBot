5057aa11943f03f433541fe03615f15bc8176810
Is'haq Abdulrazzaq <ishaqat111@gmail.com>
2024-07-15 16:53:57 +0100

    Change port from 3000 to 8000

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const chatbotRoutes = require('./routes/chatbot');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
